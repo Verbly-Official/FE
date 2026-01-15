46d6d82358b83d53243bb70e89cd5a14198fbd59
Naongjin <syje1004@naver.com>
2026-01-16 04:34:29 +0900

    [Feat/#25] tabComponent에 BtnTab 추가

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -3,12 +3,13 @@ import GNB from "./components/Nav/GNB";
 import GNB_Alarm from "./components/Nav/GNB_Alarm";
 import SideMenu from "./components/Nav/SideMenu";
 import SideMenu_Tab from "./components/Nav/SideMenu_Tab";
+import BtnTab from "./components/Tab/BtnTab";
 import Tab from "./components/Tab/Tab";
 
 function App() {
   return (
     <>
-      <Tab text="home" isSelected={true} />
+      <BtnTab />
     </>
   );
 }
