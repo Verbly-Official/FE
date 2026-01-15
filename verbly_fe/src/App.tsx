fc588ade4e6569fca37486fa75a5020fd04cb680
Naongjin <syje1004@naver.com>
2026-01-16 04:04:55 +0900

    [Feat/#27] ChipComponent

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import GNB from "./components/Nav/GNB";
-import GNB_Alarm from "./components/Nav/GNB_Alarm";
-import SideMenu from "./components/Nav/SideMenu";
-import SideMenu_Tab from "./components/Nav/SideMenu_Tab";
-import Tab from "./components/Tab/Tab";
+import Chip from "./components/Chip/Chip";
 
 function App() {
   return (
     <>
-      <Tab text="home" isSelected={true} />
+      <Chip label="how are you?" />
     </>
   );
 }
