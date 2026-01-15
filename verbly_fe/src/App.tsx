4e8486454bcad27f73fc19e07ebcd9dce5f528d1
Naongjin <syje1004@naver.com>
2026-01-16 03:43:50 +0900

    [Feat/#25] Tab component

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -3,11 +3,12 @@ import GNB from "./components/Nav/GNB";
 import GNB_Alarm from "./components/Nav/GNB_Alarm";
 import SideMenu from "./components/Nav/SideMenu";
 import SideMenu_Tab from "./components/Nav/SideMenu_Tab";
+import Tab from "./components/Tab/Tab";
 
 function App() {
   return (
     <>
-      <SideMenu variant="small" />
+      <Tab text="home" isSelected={true} />
     </>
   );
 }
