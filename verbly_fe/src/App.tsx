4d87544e2801385871903f121b094a1267a3369c
“ninininhihi” <nasasm1120@naver.com>
2026-01-18 23:11:40 +0900

    [Feat/#54] ProgressIndicator 구현

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import GradientButtonTestPage from "./pages/GradientButtonTestPage";
+import ProgressIndicatorDemo from "./pages/ProgressIndicatorDemo";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
         <Route path="/gradient-button-test" element={<GradientButtonTestPage />} />
+        <Route path="/progress-indicator" element={<ProgressIndicatorDemo />} />
       </Routes>
     </BrowserRouter>
   );
