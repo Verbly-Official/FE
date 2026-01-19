f8badd90d83cf20ea6bfeca65c954ae90353d3c2
whghdus <whghdus04@gmail.com>
2026-01-19 09:13:20 +0900

    [refactor/#39] 버튼 수정

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -1,14 +1,15 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import GradientButtonTestPage from "./pages/GradientButtonTestPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// 기존 import 유지...
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
-        <Route path="/gradient-button-test" element={<GradientButtonTestPage />} />
+        {/* 기존 라우트 유지 */}
+        <Route path="/login" element={<LoginPage />} />
+        {/* 필요시 메인 경로를 로그인으로 변경 */}
+        {/* <Route path="/" element={<LoginPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
