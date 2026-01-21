cd2881ba1bd2fe19d6e69949a39e6aca7e06714f
Naongjin <syje1004@naver.com>
2026-01-22 02:05:00 +0900

    [fix/#75]-충돌해결

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -5,22 +5,26 @@ import ProgressIndicatorDemo from "./pages/ProgressIndicatorDemo";
 import LoginPage from "./pages/LoginPage";
 import SelectLangPage from "./pages/SelectLangPage";
 import Correction_Main from "./pages/Correction/CorrectionMainPage";
-import InboxPage from "./pages/Inbox/InboxPage";
+import Test from "./pages/Home/test";
 
 function App() {
   return (
     <BrowserRouter>
       <nav className="p-2 bg-gray-100 mb-4 flex gap-4">
-        <Link to="/login" className="text-blue-500 hover:underline">Login</Link>
-        <Link to="/inbox" className="text-blue-500 hover:underline">Inbox</Link>
+        <Link to="/login" className="text-blue-500 hover:underline">
+          Login
+        </Link>
+        <Link to="/inbox" className="text-blue-500 hover:underline">
+          Inbox
+        </Link>
       </nav>
-      
+
       <Routes>
         <Route path="/progress-indicator" element={<ProgressIndicatorDemo />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/select-language" element={<SelectLangPage />} />
         <Route path="/correction" element={<Correction_Main />} />
-        <Route path="/inbox" element={<InboxPage />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
