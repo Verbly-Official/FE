ef9a6aadc88f1e0357fed763f21f45ebe971121c
kniiiiko <dywjd1128@naver.com>
2026-01-21 17:50:29 +0900

    [chore/#68] App.tsx 경로 추가

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -5,6 +5,7 @@ import GradientButtonTestPage from "./pages/GradientButtonTestPage";
 import ProgressIndicatorDemo from "./pages/ProgressIndicatorDemo";
 import LoginPage from "./pages/LoginPage";
 import SelectLangPage from "./pages/SelectLangPage";
+import Correction_Main from "./pages/Correction/CorrectionMainPage";
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
       <Routes>
         <Route path="/gradient-button-test" element={<GradientButtonTestPage />} />
         <Route path="/progress-indicator" element={<ProgressIndicatorDemo />} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/select-language" element={<SelectLangPage />} /> 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/select-language" element={<SelectLangPage />} />
+        <Route path="/correction" element={<Correction_Main />} />
       </Routes>
     </BrowserRouter>
   );
