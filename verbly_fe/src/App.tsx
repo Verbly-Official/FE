8a72eedcefc744eeeaa81f9052bab8456b7cedd6
kniiiiko <dywjd1128@naver.com>
2026-01-21 18:01:29 +0900

    [chore] App.tsx 경로 삭제

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import GradientButtonTestPage from "./pages/GradientButtonTestPage";
 import ProgressIndicatorDemo from "./pages/ProgressIndicatorDemo";
 import LoginPage from "./pages/LoginPage";
 import SelectLangPage from "./pages/SelectLangPage";
@@ -10,10 +9,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/gradient-button-test" element={<GradientButtonTestPage />} />
         <Route path="/progress-indicator" element={<ProgressIndicatorDemo />} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/select-language" element={<SelectLangPage />} /> 
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/select-language" element={<SelectLangPage />} />
       </Routes>
     </BrowserRouter>
   );
