57c08b260ab27062d0ae229d72c9f8a9ac464f4a
Naongjin <syje1004@naver.com>
2026-01-24 01:43:08 +0900

    [feat/#112] WriteModal 구현 중

--- verbly_fe/src/pages/Home/Home_Korean.tsx
+++ verbly_fe/src/pages/Home/Home_Korean.tsx
@@ -1,4 +1,5 @@
 import Home_Card from "../../components/Home/Home_Card";
+import Home_WriteModal from "../../components/Home/Home_WriteModal.tsx";
 import GNB from "../../components/Nav/GNB";
 import SideMenu from "../../components/Nav/SideMenu";
 import Tabs from "../../components/Tab/Tabs";
@@ -8,37 +9,38 @@ import { MOCK_USER_PROFILE, MOCK_USER_STATS } from "./mockData.ts";
 
 export default function Home_Korean() {
   return (
-    <>
-      <div className="min-h-screen">
-        {/*GNB*/}
-        <div className="w-full mx-auto">
-          <GNB variant="home" />
-        </div>
-        <div>
-          <div className="w-full min-h-screen bg-bg0 flex flex-row justify-between pr-[40px]">
-            <SideMenu />
-            <div className="bg-white p-[24px] w-[1120px] min-h-screen mt-[32px] rounded-[12px]">
-              {/* Tab */}
-              <div className="flex mb-[28px] justify-start gap-0 border-b-[1px] border-line2">
-                <Tabs tabs={["For You", "Hot Posts"]} />
-              </div>
-              <div className="flex flex-col gap-[20px]">
-                <Home_Card varient="default" isCorrected={false} />
-                <Home_Card varient="default" isCorrected={true} />
-              </div>
+    <div className="min-h-screen">
+      {/*GNB*/}
+      <div className="w-full mx-auto">
+        <GNB variant="home" />
+      </div>
+      <div>
+        <div className="w-full min-h-screen bg-bg0 flex flex-row justify-between pr-[40px]">
+          <SideMenu />
+          <div className="absolute">
+            <Home_WriteModal variant="KOREAN" />
+          </div>
+          <div className="bg-white p-[24px] w-[1120px] min-h-screen mt-[32px] rounded-[12px]">
+            {/* Tab */}
+            <div className="flex mb-[28px] justify-start gap-0 border-b-[1px] border-line2">
+              <Tabs tabs={["For You", "Hot Posts"]} />
             </div>
-            <div>
-              <div className="mt-[32px] flex flex-col gap-[32px]">
-                <UserStatsCard
-                  userData={MOCK_USER_PROFILE}
-                  stats={MOCK_USER_STATS}
-                />
-                <TrendingTag />
-              </div>
+            <div className="flex flex-col gap-[20px]">
+              <Home_Card varient="default" isCorrected={false} />
+              <Home_Card varient="default" isCorrected={true} />
+            </div>
+          </div>
+          <div>
+            <div className="mt-[32px] flex flex-col gap-[32px]">
+              <UserStatsCard
+                userData={MOCK_USER_PROFILE}
+                stats={MOCK_USER_STATS}
+              />
+              <TrendingTag />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
