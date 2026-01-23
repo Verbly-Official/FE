8615363f2b69e7d58217a199ff2e0d90e23371b0
Naongjin <syje1004@naver.com>
2026-01-24 00:41:29 +0900

    [refactor/#102] GNB 로고로 홈 이동 구현

--- verbly_fe/src/components/Logo/Logo.tsx
+++ verbly_fe/src/components/Logo/Logo.tsx
@@ -2,32 +2,27 @@ import logoSmall from "./img/logo-small.svg";
 import logoLarge from "./img/logo-large.svg";
 import logoHori from "./img/logo-hori.svg";
 import logoVerti from "./img/logo-verti.svg";
+import { useNavigate } from "react-router-dom";
 
-export default function Logo({ variant = "small" }) {
-  switch (variant) {
-    case "small":
-      return (
-        <div>
-          <img src={logoSmall} alt="logo small" />
-        </div>
-      );
-    case "large":
-      return (
-        <div>
-          <img src={logoLarge} alt="logo large" />
-        </div>
-      );
-    case "hori":
-      return (
-        <div>
-          <img src={logoHori} alt="logo hori" />
-        </div>
-      );
-    case "verti":
-      return (
-        <div>
-          <img src={logoVerti} alt="logo verti" />
-        </div>
-      );
-  }
+type LogoVariant = "small" | "large" | "hori" | "verti";
+interface LogoProps {
+  variant?: LogoVariant;
+}
+
+const logoMap: Record<LogoVariant, string> = {
+  small: logoSmall,
+  large: logoLarge,
+  hori: logoHori,
+  verti: logoVerti,
+};
+
+export default function Logo({ variant = "small" }: LogoProps) {
+  const navigate = useNavigate();
+  const logoSrc = logoMap[variant];
+
+  return (
+    <div onClick={() => navigate("/home-korean")} className="cursor-pointer">
+      <img src={logoSrc} alt={`logo ${variant}`} />
+    </div>
+  );
 }
