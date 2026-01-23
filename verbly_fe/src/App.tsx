93fc1eeec6df5b19dfe0130b7e1531928e7b752e
Naongjin <syje1004@naver.com>
2026-01-23 22:48:07 +0900

    리베이스 전 커밋

--- verbly_fe/src/App.tsx
+++ verbly_fe/src/App.tsx
@@ -6,27 +6,15 @@ import SelectLangPage from "./pages/Onboarding/SelectLangPage";
 import Correction_Main from "./pages/Correction/CorrectionMainPage";
 import InboxPage from "./pages/Inbox/InboxPage";
 import Home_Korean from "./pages/Home/Home_Korean";
+import Home_Native from "./pages/Home/Home_Native";
 import MyPage_Korean from "./pages/My/My_Korean";
 import MyPage_Foreigner from "./pages/My/My_Foreigner";
 import Correction_Write from "./pages/Correction/WriteCorrectionPage";
-import InboxReviewPage from "./pages/Inbox/ReviewPage";
+import ReviewPage from "./pages/Inbox/ReviewPage";
 import ProfilePage from "./pages/Inbox/ProfilePage";
-<<<<<<< HEAD
 import Correction_NMain from "./pages/Correction/Native/CorrectionMainNativePage";
-<<<<<<< HEAD
-import LibraryPage from "./pages/Library/LibraryPage";
-import ReviewPage from "./pages/Library/Review/ReviewPage";
-import { ReviewResultPage } from "./pages/Library/Review/ReviewResultPage";
-import { WordDetailPage } from "./pages/Library/WordDetailPage";
-=======
 import EditProfilePage from "./pages/My/EditProfilePage";
-<<<<<<< HEAD
->>>>>>> 23a3396 (pull 받기 전 커밋)
-=======
-=======
 import PaymentPage from "./pages/My/PaymentPage";
->>>>>>> a80f4dd ([fix/#80] 수정)
->>>>>>> 201d92c ([fix/#80] 수정)
 
 function App() {
   return (
@@ -35,29 +23,11 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/login/select-language" element={<SelectLangPage />} />
         <Route path="/correction" element={<Correction_Main />} />
-        <Route path="/inbox" element={<InboxPage />} />
         <Route path="/home-korean" element={<Home_Korean />} />
+        <Route path="/home-native" element={<Home_Native />} />
         <Route path="/correction/write" element={<Correction_Write />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/review/:id" element={<InboxReviewPage />} />
-        <Route path="/profile/:id" element={<ProfilePage />} />
-        <Route path="/correction1" element={<Correction_NMain />} />
-        <Route path="/library" element={<LibraryPage />} />
-        <Route path="/library/word/:word" element={<WordDetailPage />} />
-        <Route path="/review" element={<ReviewPage />} />
-        <Route path="/review/result" element={<ReviewResultPage />} />
-=======
-=======
->>>>>>> 201d92c ([fix/#80] 수정)
-        <Route path="/mypage" element={<MyPage_Korean />} />
-=======
-        <Route path="/my-korean" element={<MyPage_Korean />} />
-        <Route path="/my-foreigner" element={<MyPage_Foreigner />} />
         <Route path="/edit-profile" element={<EditProfilePage />} />
         <Route path="/my-korean/payment" element={<PaymentPage />} />
->>>>>>> a80f4dd ([fix/#80] 수정)
         <Route path="/inbox" element={<InboxPage />} />
         <Route path="/review/:id" element={<ReviewPage />} />
         <Route path="/profile/:id" element={<ProfilePage />} />
@@ -65,7 +35,6 @@ function App() {
         <Route path="/my-korean" element={<MyPage_Korean />} />
         <Route path="/my-foreigner" element={<MyPage_Foreigner />} />
         <Route path="/edit-profile" element={<EditProfilePage />} />
->>>>>>> 23a3396 (pull 받기 전 커밋)
       </Routes>
     </BrowserRouter>
   );
---
+++ verbly_fe/src/pages/Home/Home_Native.tsx
@@ -0,0 +1,39 @@
+import Home_Card from "../../components/Home/Home_Card";
+import GNB from "../../components/Nav/GNB";
+import SideMenu from "../../components/Nav/SideMenu";
+import Tabs from "../../components/Tab/Tabs";
+import TrendingTag from "../../components/TrendingTag/TrendingTag";
+export default function Home_Native() {
+  return (
+    <>
+      <div className="min-h-screen">
+        {/*GNB*/}
+        <div className="w-full mx-auto">
+          <GNB variant="home" />
+        </div>
+        <div>
+          <div className="w-full min-h-screen bg-bg0 flex flex-row justify-between pr-[40px]">
+            <SideMenu />
+
+            <div className="bg-white p-[24px] w-[1120px] min-h-screen mt-[32px] rounded-[12px]">
+              {/* Tab */}
+              <div className="flex mb-[28px] justify-start gap-0">
+                <Tabs tabs={["For You", "Hot Posts"]} />
+              </div>
+              <div className="flex flex-col gap-[20px]">
+                <Home_Card varient="default" isCorrected={false} />
+                <Home_Card varient="default" isCorrected={true} />
+              </div>
+            </div>
+            <div>
+              <div className="mt-[32px] flex flex-col gap-[32px]">
+                <div className="bg-white w-[384px] h-[503px]">Profile</div>
+                <TrendingTag />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
