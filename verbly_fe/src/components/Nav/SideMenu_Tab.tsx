20035fac380ada8af9125463820793684adba57e
Naongjin <syje1004@naver.com>
2026-01-23 12:18:41 +0900

    [fix/#102] 1차 수정

--- verbly_fe/src/components/Nav/SideMenu_Tab.tsx
+++ verbly_fe/src/components/Nav/SideMenu_Tab.tsx
@@ -1,21 +1,23 @@
 type SideMenuProps = {
   label: string;
   isSelected: boolean;
-  onClick: () => void;
-};
+} & React.HTMLAttributes<HTMLDivElement>;
 export default function SideMenu_Tab({
   label,
   isSelected = false,
-  onClick,
+  className,
+  ...props
 }: SideMenuProps) {
   return (
     <div
-      onClick={onClick}
+      {...props}
       className={`w-[172px] h-[56px] flex items-center gap-[20px] px-[16px] rounded-[4px] text-[20px] cursor-pointer ${
         isSelected
           ? "bg-violet-100 text-violet-50 border-[1px] border-violet-50"
           : "bg-white"
-      }`}
+      }
+      ${className ?? ""}
+      `}
     >
       <img
         src={`${
