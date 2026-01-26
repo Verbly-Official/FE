f4aac51ed8220c68d0e5237710ec0d81d717fa52
kniiiiko <dywjd1128@naver.com>
2026-01-26 16:34:27 +0900

    [feat/#87] btnTab 영역 추가

--- verbly_fe/src/pages/Correction/WriteCorrectionPage.tsx
+++ verbly_fe/src/pages/Correction/WriteCorrectionPage.tsx
@@ -6,6 +6,7 @@ import { OutlinedButton } from "../../components/Button";
 import Sidebar from "./SideBar";
 import { TextField } from "../../components/TextArea/TextField";
 import TextArea from "../../components/TextArea/TextArea";
+import PlusSrc from "../../assets/emoji/plus.svg";
 
 const Correction_Write = () => {
   const [text, setText] = useState("");
@@ -46,7 +47,7 @@ const Correction_Write = () => {
           <div className="flex">
             {/* 왼쪽 메뉴 카드 */}
             <div className="w-[278px] h-[940px] px-[1.25rem] py-9 bg-white border border-r-0 border-[#E5E7EB]">
-              <SolidButton size="large" className="w-full mb-8" label="새 글 작성하기" />
+              <SolidButton size="large" className="w-full mb-8" iconSrc={PlusSrc} label="새 글 작성하기" />
               <Sidebar />
             </div>
 
@@ -59,11 +60,13 @@ const Correction_Write = () => {
                 <TextField placeholder="" />
               </div>
 
-              <TextArea header="마크다운에디터" value={text} rows={18} onChange={(e) => setText(e.target.value)} maxRows={20} />
+              <div className="w-full">
+                <TextArea header="마크다운에디터" value={text} rows={18} onChange={(e) => setText(e.target.value)} maxRows={20} maxWidth="" />
+              </div>
             </div>
 
             {/* AI section */}
-            <div className="flex flex-col flex-1 px-[2.5rem] py-6 min-w-[30rem] rounded-r-[0.75rem] border border-[#E5E7EB] bg-[#FBF9FF] items-start gap-4">
+            <div className="flex flex-col flex-1 px-[2.5rem] py-6 rounded-r-[0.75rem] border border-[#E5E7EB] bg-[#FBF9FF] items-start gap-4">
               <div className="text-black font-pretendard text-[1.75rem] font-bold leading-none bg-[linear-gradient(90deg,#713DE3_0%,#4F46DD_100%)] bg-clip-text text-transparent">AI 도우미</div>
 
               {/* 세 버튼 - 토글 */}
