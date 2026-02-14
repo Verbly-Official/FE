909fd0dc1ea09a8b3556699000b5c3d2b3410739
Naongjin <syje1004@naver.com>
2026-02-14 20:04:40 +0900

    [feat/#135] Trending Tags에 View more 기능 추가

--- src/components/TrendingTag/TrendingTag.tsx
+++ src/components/TrendingTag/TrendingTag.tsx
@@ -6,6 +6,7 @@ import Tag from "./Tag";
 
 export default function TrendingTag() {
   const [tags, setTags] = useState<Trending[]>([]);
+  const [viewMore, setViewMore] = useState(false);
 
   useEffect(() => {
     const fetchTags = async () => {
@@ -20,19 +21,24 @@ export default function TrendingTag() {
     fetchTags();
   }, []);
 
+  const visibleTags = viewMore ? tags.slice(0, 10) : tags.slice(0, 5);
+
   return (
     <div className="flex flex-col p-[24px] gap-[16px] w-full h-stretch bg-white rounded-[8px] border-[1px] border-line1">
       <div className="text-title2-bold28 text-gray-10">Trending Tags</div>
       <div className="flex flex-col gap-[32px]">
-        {tags.map((tag) => (
+        {visibleTags.map((tag) => (
           <Tag key={tag.tagId} tag={tag} />
         ))}
       </div>
 
-      {/* w-[336px] -> w-full로 변경하여 가운데 정렬 보장 */}
-      <div className="flex justify-center w-full cursor-pointer">
-        <TextButton>View More</TextButton>
-      </div>
+      {tags.length > 5 && (
+        <div className="flex justify-center w-full cursor-pointer">
+          <TextButton onClick={() => setViewMore((prev) => !prev)}>
+            {viewMore ? "View Less" : "View More"}
+          </TextButton>
+        </div>
+      )}
     </div>
   );
 }
