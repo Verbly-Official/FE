338518d2bae675b38a0d5c5a53f38bdbbb830f21
Naongjin <syje1004@naver.com>
2026-02-19 03:26:08 +0900

    [style/#135] Trending Tag 말 줄임

--- src/components/TrendingTag/Tag.tsx
+++ src/components/TrendingTag/Tag.tsx
@@ -6,8 +6,10 @@ interface TrendingItemProps {
 
 export default function Tag({ tag }: TrendingItemProps) {
   return (
-    <div className="flex flex-col gap-[4px] cursor-pointer">
-      <div className="text-subtitle-semi22 text-gray-9">#{tag.tagName}</div>
+    <div className="flex flex-col gap-[4px] w-full">
+      <div className="text-subtitle-semi22 text-gray-9 truncate">
+        #{tag.tagName}
+      </div>
       <div className="text-body-medium14 text-gray-5">{tag.count} posts</div>
     </div>
   );
