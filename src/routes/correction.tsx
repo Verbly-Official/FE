054886593e2bd15273794cc45f26ed46a9da7f62
kniiiiko <dywjd1128@naver.com>
2026-02-19 23:03:48 +0900

    [feat/#150] korean correction 상세 페이지 추가

--- src/routes/correction.tsx
+++ src/routes/correction.tsx
@@ -4,8 +4,6 @@ import CorrectionWriteLayout from "../pages/Correction/layout/CorrectionWriteLay
 
 import Correction_Main from "../pages/Correction/korean/CorrectionMainPage";
 import Correction_Drafts from "../pages/Correction/korean/CorrectionDrafts";
-import Correction_ReadOnly from "../pages/Correction/korean/CorrectionReadOnly";
-
 import Correction_Write from "../pages/Correction/korean/WriteCorrectionPage";
 
 import Correction_NMain from "../pages/Correction/native/CorrectionMainNativePage";
@@ -14,37 +12,38 @@ import Correction_NList from "../pages/Correction/native/CorrectionListNativePag
 import CorrectionKoLayout from "../pages/Correction/layout/CorrectionKoLayout";
 import CorrectionNativeLayout from "../pages/Correction/layout/CorrectionEnLayout";
 import CorrectionRedirectPage from "../pages/Correction/CorrectionRedirect";
+import CorrectionListPage from "../pages/Correction/korean/CorrectionList";
 
 export const correctionRoutes: RouteObject[] = [
   {
     path: "correction",
     element: <CorrectionMainLayout />,
     children: [
-      /* ===== KO ===== */
+      /* ===== KO (사이드바 있는 구간) ===== */
       {
         element: <CorrectionKoLayout />,
         children: [
-          { path: "ko", element: <Correction_Main /> }, // /correction/ko
-          { path: "draft", element: <Correction_Drafts /> }, // /correction/draft
-          { index: true, element: <CorrectionRedirectPage /> }, // /correction -> redirect
-          { path: "list/:correctionId", element: <Correction_ReadOnly /> },
+          { path: "ko", element: <Correction_Main /> },
+          { path: "draft", element: <Correction_Drafts /> },
+          { index: true, element: <CorrectionRedirectPage /> },
         ],
       },
+
+      { path: "list/:correctionId", element: <CorrectionListPage /> },
 
       /* ===== NATIVE ===== */
       {
         path: "native",
         element: <CorrectionNativeLayout />,
         children: [
-          { index: true, element: <Correction_NMain /> }, // /correction/native
-          { path: "list", element: <Correction_NList /> }, // /correction/native/list
-          { path: "list/:correctionId", element: <Correction_NList /> }, // /correction/native/list/123
+          { index: true, element: <Correction_NMain /> },
+          { path: "list", element: <Correction_NList /> },
+          { path: "list/:correctionId", element: <Correction_NList /> },
         ],
       },
     ],
   },
 
-  /* ===== WRITE ===== */
   {
     path: "correction/write",
     element: <CorrectionWriteLayout />,
